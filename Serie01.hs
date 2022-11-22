80c4b8492531e54f150ca7aa95ba5f43b56387a8
Ole <37940650+olebergens@users.noreply.github.com>
2022-11-22 17:01:38 +0100

    Add files via upload

--- Serie01.hs
+++ Serie01.hs
@@ -1,4 +1,9 @@
 import Data.List (sort)
+
+-- Ole Bergens, 221200097
+-- Blazej Schott, 221200610
+-- Antonin Gräser, 221201792
+
 -- Aufgabe 1
 fourEqual :: Integer -> Integer -> Integer -> Integer -> Bool
 fourEqual a b c d = a == b && b == c && c == d
@@ -8,11 +13,25 @@ orderTriple :: Ord Integer => (Integer, Integer, Integer) -> (Integer, Integer, 
 orderTriple (x, y, z) = 
 	let [a, b , c] = sort [x, y, z]
 	in (a, b, c)
-	
-
 
 -- Aufgabe 3
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib(n-1) + fib(n-2)
+
+-- Aufgabe 4
+xs :: [Integer]
+xs = [1,2,3,4,5,6,7,8,9,10]
+
+isElem :: Integer -> [Integer] -> Bool
+isElem e [] = False
+isElem e (x:xs) 
+	| e == x = True 
+	| otherwise = isElem e xs
+
+-- Aufgabe 5
+luhnDouble :: Integer -> Integer
+luhnDouble x = if (2 * x) > 9 then (2*x)-9 else (2*x)
+luhn :: Integer -> Integer -> Integer -> Integer -> Bool
+luhn a b c d = 0 == sum[luhnDouble a, b, luhnDouble c, d] `mod` 10
